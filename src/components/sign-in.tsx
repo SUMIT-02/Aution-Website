b5409b1bce87a0d225108f00f257df7d3d4d1d7c
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-20 23:45:54 +0530

    auth

---
+++ src/components/sign-in.tsx
@@ -0,0 +1,32 @@
+
+// import { signIn } from "@/auth"
+// import { Button } from "./ui/button"
+ 
+// export default function SignIn() {
+//   return (
+//     <form
+//       action={async () => {
+//         "use server"
+//         await signIn("google")
+//       }}
+//     >
+//       <Button type="submit">Signin with Google </Button>
+//     </form>
+//   )
+// } 
+
+import { signIn } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+export function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <Button type="submit">Signin with Google</Button>
+    </form>
+  );
+}
