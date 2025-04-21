4f21e7c66627d13c00ac89f9c812d135b4c19cfa
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-22 02:12:05 +0530

    post items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,14 @@ export default async function HomePage() {
   const user= session.user
   if(!user) return null;
 
-  return <main className="container mx-auto py-12">
-    {session ? <SignOut/> :<SignIn/>}
+  return <main className="container mx-auto py-12 space-y-8">
+    {/* {session ? <SignOut/> :<SignIn/>}
 
-    {session?.user?.name}
-    
+    {session?.user?.name}       //managed in headers
+     */}
+     <h1 className="text-4xl font-bold">Post an Item to Sell</h1>
     <form 
+    className="flex flex-col border p-8 rounded-xl space-y-4 max-w-lg"
     action={async(formData:FormData)=>{
       "use server";
       //const bid=formData.get("bid") as string;
@@ -32,11 +34,14 @@ export default async function HomePage() {
       })
       revalidatePath("/");
     }}>
-      <Input name="name" placeholder="Name Your Items"/>
+      <Input className="max-w-md" name="name" placeholder="Name Your Items"/>
       <Button type="submit">Post Item </Button>
     </form>
+
+    <h2 className="text-2xl font-bold">Items For Sale</h2>
+    <div className="grid grid-cols-4 gap-8">
     {allItems.map((item)=>(
-      <div key={item.id}>{item.name}</div>
+      <div key={item.id} className="border p-8 rounded-xl">{item.name}</div>
 
     //   <Input name="name" placeholder="Name Your Items"/>
     //   <Button type="submit">Post Item </Button>
@@ -45,6 +50,7 @@ export default async function HomePage() {
     //   <div key={item.id}>{item.name}</div>
 
     ))}
+    </div>
   </main>
  
 }
