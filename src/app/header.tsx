59d824b51d2810610a3211951b3dee90cadddff9
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-25 23:24:22 +0530

    linting

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,33 +1,36 @@
 import { auth } from "@/auth";
-import { SignIn } from "@/components/sign-in"
-import { SignOut } from "@/components/sign-out"
-import  Link  from "next/link";
-import Image from "next/image"
-export async function Header(){
-    const session = await auth(); 
+import { SignIn } from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+import Link from "next/link";
+import Image from "next/image";
+export async function Header() {
+  const session = await auth();
 
-    return (
+  return (
     <div className="bg-gray-200 py-4">
-        <div className="container flex justify-between" >
-            
-               <div className="flex items-center gap-12 ">
-               <Link href="/" className="font-bold hover:underline flex items-center gap-1 ">
-                {/* <Image src="/logo.png" width="50" height="50" alt="Logo"/> */}
-                Bigbuddy.com
-                </Link>
-                <div>
-                <Link href="/items/create" 
-                className="font-bold hover:underline flex items-center gap-1">
-               Auction Item
-                </Link>
-                </div>
-                
-               </div>
-            <div className="flex items-center gap-4">
-            <div>{session?.user?.name}</div>
-            <div>{session?<SignOut/>:<SignIn/>}</div>
-            </div>
+      <div className="container flex justify-between">
+        <div className="flex items-center gap-12 ">
+          <Link
+            href="/"
+            className="font-bold hover:underline flex items-center gap-1 "
+          >
+            {/* <Image src="/logo.png" width="50" height="50" alt="Logo"/> */}
+            Bigbuddy.com
+          </Link>
+          <div>
+            <Link
+              href="/items/create"
+              className="font-bold hover:underline flex items-center gap-1"
+            >
+              Auction Item
+            </Link>
+          </div>
         </div>
+        <div className="flex items-center gap-4">
+          <div>{session?.user?.name}</div>
+          <div>{session ? <SignOut /> : <SignIn />}</div>
+        </div>
+      </div>
     </div>
-    ) 
+  );
 }
