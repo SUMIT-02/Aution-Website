f676fdae91565adaed6486a6a9b7f8d5d7097610
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-25 23:24:35 +0530

    linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,33 @@
 import { database } from "@/db/database";
-import { bids as bidsSchema, items} from "@/db/schema";
+import { bids as bidsSchema, items } from "@/db/schema";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { revalidatePath } from "next/cache";
-import {SignIn} from "@/components/sign-in";
+import { SignIn } from "@/components/sign-in";
 import { SignOut } from "@/components/sign-out";
 import { auth } from "@/auth";
 
-
 export default async function HomePage() {
-  const session =await auth();
+  const session = await auth();
 
-  const allItems=await database.query.items.findMany();
+  const allItems = await database.query.items.findMany();
   if (!session) return null;
 
-  const user= session.user
-  if(!user) return null;
+  const user = session.user;
+  if (!user) return null;
 
-  return <main className="container mx-auto py-12 space-y-8">
-   
-     <h1 className="text-4xl font-bold">Item for Sell</h1>
-    
-    <div className="grid grid-cols-4 gap-8">
-    {allItems.map((item)=>(
-      <div key={item.id} className="border p-8 rounded-xl">
-        {item.name}
-        starting price:${item.startingPrice/100}
-        </div>
-      
-    
-    ))}
-    </div>
-  </main>
- 
+  return (
+    <main className="container mx-auto py-12 space-y-8">
+      <h1 className="text-4xl font-bold">Item for Sell</h1>
+
+      <div className="grid grid-cols-4 gap-8">
+        {allItems.map((item) => (
+          <div key={item.id} className="border p-8 rounded-xl">
+            {item.name}
+            starting price:${item.startingPrice / 100}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
