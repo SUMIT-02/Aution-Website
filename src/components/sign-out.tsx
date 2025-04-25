42df8d6b3850eadab1f7bbff1b024c35cd8c2773
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-25 23:25:19 +0530

    auth changes

--- src/components/sign-out.tsx
+++ src/components/sign-out.tsx
@@ -1,15 +1,15 @@
-import { signOut } from "@/auth"
-import { Button } from "./ui/button"
- 
+import { signOut } from "@/auth";
+import { Button } from "./ui/button";
+
 export function SignOut() {
   return (
     <form
       action={async () => {
-        "use server"
-        await signOut()
+        "use server";
+        await signOut();
       }}
     >
       <Button type="submit">Sign Out</Button>
     </form>
-  )
+  );
 }
