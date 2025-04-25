00f5d64985312bc63b8b1c669e6007f015895856
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-04-25 23:24:41 +0530

    linting

--- src/app/items/create/actions.ts
+++ src/app/items/create/actions.ts
@@ -1,27 +1,25 @@
-"use server"
+"use server";
 
 import { database } from "@/db/database";
 import { items, sessions } from "@/db/schema";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
+export async function createItemActions(formData: FormData) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  const user = session.user;
+  if (!user || !user.id) {
+    throw new Error("Unauthorized");
+  }
 
-export async function createItemActions(formData:FormData) {
-        const session= await auth();
-        if (!session){
-          throw new Error("Unauthorized")
-        }
-        const user=session.user;
-        if (!user || !user.id){
-          throw new Error("Unauthorized")
-        }
-
-        await database.insert(items).values({
-          name:formData.get("name") as string,
-          startingPrice:Number(formData.get("startingPrice")),
-          userId:user.id,
-        })
-        // revalidatePath("/");
-        redirect("/");
-    
+  await database.insert(items).values({
+    name: formData.get("name") as string,
+    startingPrice: Number(formData.get("startingPrice")),
+    userId: user.id,
+  });
+  // revalidatePath("/");
+  redirect("/");
 }
