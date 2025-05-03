e67244a4d8f1b87f20a619da8606273f6ae2961b
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-05-03 23:52:30 +0530

    upload file to R2

--- src/app/items/create/actions.ts
+++ src/app/items/create/actions.ts
@@ -4,8 +4,21 @@ import { database } from "@/db/database";
 import { items, sessions } from "@/db/schema";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { getSignedUrlForS3Object } from "@/lib/s3";
 
-export async function createItemActions(formData: FormData) {
+export async function createUploadUrlAction(key: string, type: string) {
+  return await getSignedUrlForS3Object(key, type);
+}
+
+export async function createItemAction({
+  fileName,
+  name,
+  startingPrice,
+}: {
+  fileName: string;
+  name: string;
+  startingPrice: number;
+}) {
   const session = await auth();
   if (!session) {
     throw new Error("Unauthorized");
@@ -16,8 +29,9 @@ export async function createItemActions(formData: FormData) {
   }
 
   await database.insert(items).values({
-    name: formData.get("name") as string,
-    startingPrice: Number(formData.get("startingPrice")),
+    name,
+    startingPrice,
+    fileKey: fileName,
     userId: user.id,
   });
   // revalidatePath("/");
