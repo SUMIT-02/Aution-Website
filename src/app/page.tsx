a2e899fe443863ec7d2af584b92372e39344679b
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-05-03 23:30:10 +0530

    upload file to R2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
 import { database } from "@/db/database";
-import { bids as bidsSchema, items } from "@/db/schema";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { revalidatePath } from "next/cache";
-import { SignIn } from "@/components/sign-in";
-import { SignOut } from "@/components/sign-out";
-import { auth } from "@/auth";
+import { ItemCard } from "./items-card";
 
 export default async function HomePage() {
-  const session = await auth();
-
   const allItems = await database.query.items.findMany();
-  if (!session) return null;
-
-  const user = session.user;
-  if (!user) return null;
 
   return (
     <main className="container mx-auto py-12 space-y-8">
@@ -22,10 +10,7 @@ export default async function HomePage() {
 
       <div className="grid grid-cols-4 gap-8">
         {allItems.map((item) => (
-          <div key={item.id} className="border p-8 rounded-xl">
-            {item.name}
-            starting price:${item.startingPrice / 100}
-          </div>
+          <ItemCard key={item.id} item={item} />
         ))}
       </div>
     </main>
