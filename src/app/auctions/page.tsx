65a5feda19c01caadbd7691e604983efe0303169
SUMIT-02 <patilsumitkumar22@gmail.com>
2025-05-14 19:52:55 +0530

    post an item

--- src/app/auctions/page.tsx
+++ src/app/auctions/page.tsx
@@ -1,18 +1,33 @@
 import { database } from "@/db/database";
 import { ItemCard } from "@/app/items-card";
+import { auth } from "@/auth";
+import { items } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { EmptyState } from "./empty-state";
 
 export default async function MyAuctionPage() {
-  const allItems = await database.query.items.findMany();
+  const session = await auth();
+  if (!session || !session.user) {
+    throw new Error("Unauthorized");
+  }
+  const allItems = await database.query.items.findMany({
+    where: eq(items.userId, session.user.id!),
+  });
+  const hasItems = allItems.length > 0;
 
   return (
     <main className="container mx-auto py-12 space-y-8">
-      <h1 className="text-4xl font-bold">Item for Sell</h1>
+      <h1 className="text-4xl font-bold">Your Current Auctions</h1>
 
-      <div className="grid grid-cols-4 gap-8">
-        {allItems.map((item) => (
-          <ItemCard key={item.id} item={item} />
-        ))}
-      </div>
+      {hasItems ? (
+        <div className="grid grid-cols-4 gap-8">
+          {allItems.map((item) => (
+            <ItemCard key={item.id} item={item} />
+          ))}
+        </div>
+      ) : (
+        <EmptyState />
+      )}
     </main>
   );
 }
